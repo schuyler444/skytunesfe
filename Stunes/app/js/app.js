e71a0df30ddba27c4f50dd413cc57257916fd14b
Schuyler Levinson <sbl72@cornell.edu>
2016-03-05 20:10:36 -0500

    working music player and album display

--- Stunes/app/js/app.js
+++ Stunes/app/js/app.js
@@ -14,42 +14,21 @@ skytunes.config(function($routeProvider){
 var playing = false;
 
 skytunes.controller('skytunesController', function($scope,$http, player) {
-	$http.get("artists.json")
+
+    $http.get("artists.json")
     .then(function(response) {
         $scope.artists= response.data;
     });
 
     $scope.player = player;
-
-/*
-    $scope.songSelect = function(songPath) {
-    	$scope.selectedSongPath = songPath;
-	}
-	//var audio = document.getElementById("mainAudio");*/
-/*
-	$scope.playing = false;
-	$scope.playlist = [];
-	$scope.play = function(){
-		if($scope.playing == false){
-			var audio = document.getElementById("mainAudio");
-			audio.play();
-			$scope.playing= true;
-		}
-	}
-	$scope.pause = function(){
-		if ($scope.playing == true){
-			var audio = document.getElementById("mainAudio");
-			audio.pause();
-			$scope.playing = false;
-		}
-	}
-*/
+   
 });
 
   skytunes.factory('player', function(audio, $rootScope) {
     var player,
         playlist = [],
         paused = false,
+        artist = [];
         current = {
           album: 0,
           track: 0
@@ -58,17 +37,20 @@ skytunes.controller('skytunesController', function($scope,$http, player) {
     player = {
       playlist: playlist,
 
+      artist: artist,
+
       current: current,
 
       playing: false,
 
-      play: function(track, album) {
+      play: function() {
         if (!playlist.length) return;
 
-        if (angular.isDefined(track)) current.track = track;
-        if (angular.isDefined(album)) current.album = album;
+        //if (angular.isDefined(track)) current.track = track;
+        //if (angular.isDefined(album)) current.album = album;
+        //console.log(playlist[0].albums[current.album]);
 
-        if (!paused) audio.src = playlist[current.album].tracks[current.track].url;
+        if (!paused) audio.src = playlist[0].albums[current.album].tracks[current.track].url;
         audio.play();
         player.playing = true;
         paused = false;
@@ -91,38 +73,66 @@ skytunes.controller('skytunesController', function($scope,$http, player) {
       next: function() {
         if (!playlist.length) return;
         paused = false;
-        if (playlist[current.album].tracks.length > (current.track + 1)) {
-          current.track++;
+        if (playlist[0].albums[current.album].tracks.length < (current.track + 2)) {
+          if (playlist[0].albums.length < (current.album +2) ){
+            playlist.pop();
+            audio.pause();
+            audio.src = "";
+            player.playing = false;
+            paused = true;
+          }else {
+            current.album++;
+            current.track = 0;
+          }
         } else {
-          current.track = 0;
-          current.album = (current.album + 1) % playlist.length;
+          current.track++;
         }
+
         if (player.playing) player.play();
+
       },
 
       previous: function() {
         if (!playlist.length) return;
         paused = false;
-        if (current.track > 0) {
+        if (current.track == 0) {
+          if (current.album == 0){
+            playlist.pop();
+            audio.pause();
+            audio.src = "";
+            player.playing = false;
+            paused = true;
+          }else{
+            current.album--;
+            current.track = playlist[0].albums[current.album].length-1;
+          }
+        } else{
           current.track--;
-        } else {
-          current.album = (current.album - 1 + playlist.length) % playlist.length;
-          current.track = playlist[current.album].tracks.length - 1;
         }
         if (player.playing) player.play();
       }
     };
 
-    playlist.add = function(album) {
-      if (playlist.indexOf(album) != -1) return;
-      playlist.push(album);
+    playlist.add = function(artist,album,track) {
+      
+      current.album = artist.albums.indexOf(album);
+      current.track = album.tracks.indexOf(track);
+      playlist.pop();
+      playlist.push(artist);
+      paused = false;
+      player.play();
     };
 
-    playlist.remove = function(album) {
+    /*playlist.remove = function(album) {
       var index = playlist.indexOf(album);
       if (index == current.album) player.reset();
       playlist.splice(index, 1);
-    };
+    };*/
+
+    artist.set = function(artst) {
+      artist.pop();
+      artist.push(artst);
+    }
 
     audio.addEventListener('ended', function() {
       $rootScope.$apply(player.next);
@@ -137,30 +147,3 @@ skytunes.controller('skytunesController', function($scope,$http, player) {
     var audio = $document[0].createElement('audio');
     return audio;
   });
-
-/*
-skytunes.controller('SidebarController', function($scope) {
-
-    $scope.state = false;
-
-    $scope.toggleState = function() {
-        $scope.state = !$scope.state;
-    };
-
-});
-
-skytunes.directive('sidebarDirective', function() {
-    return {
-        link : function(scope, element, attr) {
-            scope.$watch(attr.sidebarDirective, function(newVal) {
-                  if(newVal)
-                  {
-                    element.addClass('show');
-                    return;
-                  }
-                  element.removeClass('show');
-            });
-        }
-    };
-});
-*/
